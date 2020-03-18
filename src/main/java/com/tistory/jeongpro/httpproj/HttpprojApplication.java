d7d49bd7d68df17b7ee5a9052570a0610a14d648
Dong gyo Jeong <ehdrylang@naver.com>
2020-03-18 21:06:22 +0900

    init project

---
+++ src/main/java/com/tistory/jeongpro/httpproj/HttpprojApplication.java
@@ -0,0 +1,13 @@
+package com.tistory.jeongpro.httpproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpprojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tistory/jeongpro/httpproj/HttpprojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tistory.jeongpro.httpproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpprojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
